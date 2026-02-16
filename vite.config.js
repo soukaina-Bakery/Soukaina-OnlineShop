839c69ee7bb8103ba796318b39dc95329c7ae31f
Soukaina <soukainaidrissi60@gmail.com>
2026-02-16 12:12:23 +0100

    My changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bakery-soukaina/',
+  base: "/Soukaina-OnlineShop/",
   plugins: [react()],
 })
